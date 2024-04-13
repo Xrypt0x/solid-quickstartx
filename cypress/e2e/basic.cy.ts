e70a58ef11d33fc9ce8b3edaa3c7d051c63c4c07
Xrypt0x <166910989+Xrypt0x@users.noreply.github.com>
2024-04-13 21:17:37 +0530

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,7 +5,7 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
+    .contains('Jus a useless site/Faltu ki bakchodi h', { matchCase: false })
   })
 
   it('displays Click button', () => {
